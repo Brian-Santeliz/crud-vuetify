f3a5f637cc15b9c52d45c8542e0bcc4eb4573d80
Brian <blsvsanteliz@gmail.com>
2021-05-17 21:30:29 -0400

    Delete componente helloWorl, create basic layout2

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,51 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-main>
-      <HelloWorld/>
-    </v-main>
+    <header>
+      <h2 class="text-center green white--text">CRUD Vuejs | Actividad I</h2>
+    </header>
+    <v-container>
+      <v-row class="flex align-center justify-space-between my-5">
+        <v-col class="justify-end" cols="12 text-center" sm="4">
+          <h3>
+            Total Construcciones:
+            <span class="count">8</span>
+          </h3>
+        </v-col>
+        <v-col class="justify-start" cols="12 text-center" sm="4">
+          <v-btn color="green ligth white--text" elevation="2" fab>
+            <v-icon dark x-large> mdi-plus-circle </v-icon>
+          </v-btn>
+        </v-col>
+      </v-row>
+    </v-container>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
-
 export default {
-  name: 'App',
-
-  components: {
-    HelloWorld,
-  },
+  name: "App",
 
   data: () => ({
     //
   }),
 };
 </script>
+<style scoped>
+#app {
+  background-color: #ebebeb;
+}
+.count {
+  display: inline-block;
+  padding: 0.25em 0.4em;
+  font-size: 75%;
+  font-weight: 700;
+  font-size: 1rem;
+  line-height: 1;
+  text-align: center;
+  white-space: nowrap;
+  vertical-align: baseline;
+  border-radius: 0.25rem;
+  color: #fff;
+  background-color: #28a745;
+}
+</style>
